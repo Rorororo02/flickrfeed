0d1cd852839e59267e7527d1425bbdfc55dead6d
Roangelo Lacamento <roan.lacamento@gmail.com>
2015-01-06 23:53:16 +0800

    added search functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import abort, Flask, jsonify, render_template, request
 from feedparser import parse as feedparse
 
 app = Flask(__name__)
@@ -6,8 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     feed = feedparse('https://api.flickr.com/services/feeds/photos_public.gne')
-
     return render_template('index.html', feed=feed)
+
+@app.route('/_search/')
+def search():
+    tags = request.args.get('tags', '')
+    feed = feedparse('https://api.flickr.com/services/feeds/photos_public.gne?tags=%s' % tags)
+
+    # render page in case the search is not via ajax
+    if not request.is_xhr:
+        return render_template('index.html', feed=feed)
+
+    contents = []
+    for entry in feed.entries:
+        rendered = render_template("entry.html", entry=entry)
+        contents.append(rendered)
+
+    return jsonify({'contents': " ".join(contents)})
 
 
 if __name__ == '__main__':
